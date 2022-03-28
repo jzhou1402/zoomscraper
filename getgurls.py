2a056898aa82e497c746f742a88cab921281e25e
John Zhou <60801262+jzhou1402@users.noreply.github.com>
2022-03-28 18:01:52 -0400

    Update getgurls.py

--- getgurls.py
+++ getgurls.py
@@ -1,5 +1,6 @@
 """Perform a Google search using Selenium and a headless Chrome browser."""
 import subprocess
+import nltk
 from pathlib import Path
 
 import selenium
@@ -8,6 +9,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
+
+out2 = open("webinfo2.csv", "w")
 
 def test_selenium_hello():
     """Perform a Google search using Selenium and a headless Chrome browser."""
@@ -42,8 +45,10 @@ def test_selenium_hello():
     out = open("urls.txt", "w")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
+    stopwords = ["i", "me", "my", "myself", "all", "get", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 
     for i in file1:
+        out2.write(i)
         driver.implicitly_wait(1)
         driver.get("https://www.google.com")
         driver.implicitly_wait(1)
@@ -52,11 +57,38 @@ def test_selenium_hello():
         input_element.send_keys(i)  
         print("reached results")
         driver.implicitly_wait(1)
-        results = driver.find_element(By.XPATH, '//div[@class="g"]//a//h3')
-        soup = BeautifulSoup(results)
-        href_tags = soup.find_all(href=True)
-        for i in href_tags:
-            print(i)
+        try: 
+            driver.find_element(By.TAG_NAME, "cite").click()
+            resp = driver.find_element(By.XPATH, "//body").get_attribute('outerHTML')
+            soup = BeautifulSoup(resp, 'html.parser')
+            text = soup.find_all(text=True)
+            # kill all script and style elements
+            for script in soup(["script", "style"]):
+                script.extract()
+
+            # get text
+            text = soup.get_text()
+            #break into lines and remove leading and trailing space on each
+            lines = (line.strip() for line in text.splitlines())
+            # break multi-headlines into a line each
+            chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+            # drop blank lines
+            text = '\n'.join(chunk for chunk in chunks if chunk if chunk.isalnum)
+            #get each word in lowercase
+            word = "" 
+            for i in text:
+                if i == ' ' or i == '\n':
+                    if len(word) >= 3 and word.isalpha():
+                        if word not in stopwords: 
+                            out2.write(word.lower())
+                            out2.write(", ")
+                    word = ""
+                else:
+                    word += i
+
+            out2.write('\n')
+        except:
+            out2.write("not found\n")
 
     driver.quit()
 
